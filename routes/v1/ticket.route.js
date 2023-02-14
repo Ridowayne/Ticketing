0cc0ff8b126c8e9b07960f93291947e73647cca9
ridowayne <hassanrilwan12@gmail.com>
2023-02-14 01:27:07 +0100

    feat: Added user ticket routes and user routes

--- routes/v1/ticket.route.js
+++ routes/v1/ticket.route.js
@@ -1,4 +1,8 @@
 const express = express();
 const router = express.Router();
-
+const ticketController = require("../../controllers/ticket.controler");
+router.route("/reserveTicket").post(ticketController.bookTicket);
+router.route("/id").get(ticketController.getOneTicket);
+router.route("/myTicket").get(ticketController.getUserTicket);
+router.route("/reservedTickets").get(ticketController.getBokedTicket);
 module.exports = router;
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -1,4 +1,7 @@
 const express = express();
 const router = express.Router();
+const userController = require("../../controllers/user.controler");
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
 
 module.exports = router;
