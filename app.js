cc3e1dc272a0ea925e0ec909396b920ee5a04cf0
ridowayne <hassanrilwan12@gmail.com>
2023-02-14 01:54:27 +0100

    feat: updated the app set up

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const express = require("express");
-const routes = require("./routes/index");
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const compression = require('compression');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const routes = require('./routes/index');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(mongoSanitize());
+app.use(xss());
+
+app.use('/api', routes);
+app.all('/*', (req, res) => {
+    return res.status(404).json({
+        status: 'Not Found',
+        message: `can not get ${req.originalUrl} on the server`,
+    });
+});
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const app = require("./app");
-const mongoose = require("mongoose");
+const app = require('./app');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
-  console.log(`app running on ${port}`);
+    console.log(`app running on ${port}`);
 });
 const db = process.env.DB;
-mongoose.connect(db).then(() => console.log("db connected successfully"));
+mongoose.connect(db).then(() => console.log('db connected successfully'));
+
+process.on('uncaughtException', (err) => {
+    console.error(err && err.stack);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error(err && err.stack);
+});
